89631bdafc380dac705df00111b9225f34f30f6e
9santa <wotegorb@mail.ru>
2025-07-09 21:43:23 +0300

    create empty window

--- todo.c
+++ todo.c
@@ -1,7 +1,23 @@
 #include <stdio.h>
+#include <GLFW/glfw3.h>
 
 int main(void) {
-    printf("Hello, todo!\n")
+    glfwInit();
+
+    GLFWwindow* window = glfwCreateWindow(800, 600, "todo", NULL, NULL);
+
+    glfwMakeContextCurrent(window);
+
+    while(!glfwWindowShouldClose(window)) {
+        glClear(GL_COLOR_BUFFER_BIT);
+        glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
+
+        glfwPollEvents();
+        glfwSwapBuffers(window);
+    }
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
 
 
 
